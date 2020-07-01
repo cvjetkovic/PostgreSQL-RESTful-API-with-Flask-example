519e698bbaca2cebe5ff15d7dfddcc481111ea41
cvjetkovic <58811764+cvjetkovic@users.noreply.github.com>
2020-07-01 15:53:25 +0200

    crud finished

--- src/views/LokacijaView.py
+++ src/views/LokacijaView.py
@@ -13,7 +13,6 @@ def get_all():
   lokacije = LokacijaModel.get_all_lokacije_paginate(limit, offset)  
   data = lokacije_schema.dump(lokacije, many=True)
   return custom_response(data, 200)
-
   
 
 @lokacije_api.route('/<int:lokacija_id>', methods=['GET'])
@@ -38,6 +37,41 @@ def create():
   lokacija.save()
   data = lokacije_schema.dump(post)
   return custom_response(data, 201)
+
+
+@lokacije_api.route('/<int:lokacija_id>', methods=['PUT'])
+# @Auth.auth_required
+def update(lokacija_id):
+
+  req_data = request.get_json()
+  lokacija = LokacijaModel.get_one_lokacija(lokacija_id)
+  if not lokacija:
+    return custom_response({'error': 'post not found'}, 404)
+  data = lokacije_schema.dump(lokacija)
+  # if data.get('owner_id') != g.user.get('id'):
+  #   return custom_response({'error': 'permission denied'}, 400)
+  
+  data, error = lokacije_schema.load(req_data, partial=True)
+  if error:
+    return custom_response(error, 400)
+  lokacija.update(data)
+  
+  data = lokacije_schema.dump(lokacija)
+  return custom_response(data, 200)
+
+
+@lokacije_api.route('/<int:lokacija_id>', methods=['DELETE'])
+def delete(lokacija_id):
+      
+  lokacija = LokacijaModel.get_one_lokacija(lokacija_id)
+  if not lokacija:
+    return custom_response({'error': 'post not found'}, 404)
+  data = lokacije_schema.dump(lokacija)
+  # if data.get('owner_id') != g.user.get('id'):
+  #   return custom_response({'error': 'permission denied'}, 400)
+
+  lokacija.delete()
+  return custom_response({'message': 'deleted'}, 204)
 
 
 def custom_response(res, status_code):
